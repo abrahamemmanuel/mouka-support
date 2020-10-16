6c68dc57164574f69546cbbdebd437611f16b675
Abraham Emmanuel <sundayemmanuelabraham@gmail.com>
2020-10-16 10:37:57 +0100

    initial commit

--- app/Asset.php
+++ app/Asset.php
@@ -6,17 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Asset extends Model
 {
-    protected $fillable = [
-        'name', 'user_id', 'email', 'employeeno', 'laptop_name', 'laptop_model', 'laptop_serial_no','laptop_duration','remark','computer_type'
-    ];
+ protected $fillable = [
+  'name', 'user_id', 'email', 'employeeno', 'laptop_name', 'laptop_model', 'laptop_serial_no', 'laptop_duration', 'remark', 'computer_type',
+ ];
 
-    public function user()
-    {
-        return $this->belongsTo(User::class);
-    }
+ public function user()
+ {
+  return $this->belongsTo(User::class);
+ }
 
-    public function logs()
-    {
-        return $this->hasMany(Log::class);
-    }
+ public function logs()
+ {
+  return $this->hasMany(Log::class);
+ }
 }
